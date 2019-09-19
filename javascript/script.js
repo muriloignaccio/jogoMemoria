378fc8a7c798b4118eb10719bdb8f68c673a1949
Murilo Ignacio <murilohsignacio@gmail.com>
2019-09-18 22:00:23 -0300

    Criar condições para o funcionamento da lógica do jogo

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-q1 = document.getElementById("q1");
+q1 = document.getElementsByClassName("cu");
 // q2 = document.getElementById("q2");
 // q3 = document.getElementById("q3");
 // q4 = document.getElementById("q4");
@@ -17,29 +17,29 @@ q1 = document.getElementById("q1");
 
 let click = 0;
 function mudarCor(event){
-    if(click < 2){
+    if(click < 2 && event.target.style.backgroundColor != "red"){
         event.target.style.backgroundColor = "red";
-        console.log("Mudou a cor");
+        console.log(event.target.tagName);
         console.log(click);
         event.onclick = click++;
     }
 }
-q1.onclick = mudarCor;
-q2.onclick = mudarCor;
-q3.onclick = mudarCor;
-q4.onclick = mudarCor;
-q5.onclick = mudarCor;
-q6.onclick = mudarCor;
-q7.onclick = mudarCor;
+// q1.onclick = mudarCor;
+// q2.onclick = mudarCor;
+// q3.onclick = mudarCor;
+// q4.onclick = mudarCor;
+// q5.onclick = mudarCor;
+// q6.onclick = mudarCor;
+// q7.onclick = mudarCor;
 q8.onclick = mudarCor;
 q9.onclick = mudarCor;
-q10.onclick = mudarCor;
-q11.onclick = mudarCor;
-q12.onclick = mudarCor;
-q13.onclick = mudarCor;
-q14.onclick = mudarCor;
-q15.onclick = mudarCor;
-q16.onclick = mudarCor;
+// q10.onclick = mudarCor;
+// q11.onclick = mudarCor;
+// q12.onclick = mudarCor;
+// q13.onclick = mudarCor;
+// q14.onclick = mudarCor;
+// q15.onclick = mudarCor;
+// q16.onclick = mudarCor;
 
 
 
